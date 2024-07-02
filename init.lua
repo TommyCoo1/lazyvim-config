f04be965aaefc8c58afb1a3e21f0fa390995c3ae
captiveUser <captiveMail>
2024-07-02 22:59:09 +0200

    RUST LSP TEST

--- init.lua
+++ init.lua
@@ -1,2 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- CUSTOM CHANGES TOM
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { "rust_analyzer" },
+})
+
+-- config rust-analyzer with lspconfig
+require("lspconfig")["rust_analyzer"].setup({
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+      procMacro = {
+        enable = true,
+      },
+    },
+  },
+})
